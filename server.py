a544800a823811652413bfb6aeec2682e4051e3d
Prem Ganeshkumar <ganeprem@gmail.com>
2016-05-08 06:49:19 -0400

    Blacklist URLs

--- server.py
+++ server.py
@@ -2,17 +2,21 @@
 # -*- coding: utf-8 -*-
 
 import tweepy, time, sys, os, json
+import tldextract
+import logging
 from ConfigParser import SafeConfigParser
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+from tldextract.tldextract import LOG
+
 
 # Our name
 BOT_NAME = '@agolobot'
 
 # A list of sites never to summarize
 # Needs to be kept synchronized with https://github.com/premgane/agolo-slackbot/blob/master/blacklisted-sites.js
-SITE_BLACKLIST = [
+BLACKLISTED_SITES = set([
   'agolo.com',
   'youtube',
   'twitter.com',
@@ -21,7 +25,9 @@ SITE_BLACKLIST = [
   'bit.ly',
   'tinyurl',
   'vine'
-]
+])
+
+logging.basicConfig(level=logging.WARN)
 
 parser = SafeConfigParser()
 parser.read('secrets.cfg')
@@ -40,17 +46,60 @@ api = tweepy.API(auth)
 # line = "Test tweet!"
 # api.update_status(line)
 
+# Given a string, which is presumably a URL, check whether it appears in the blacklist
+def appearsInBlacklist(url):
+	extracted = tldextract.extract(url)._asdict()
+
+	print extracted
+
+	sub = ''
+	if 'subdomain' in extracted:
+		sub = extracted['subdomain']
+
+	dom = ''
+	if 'domain' in extracted:
+		dom = extracted['domain']
+
+	tld = ''
+	if 'suffix' in extracted:
+		tld = extracted['suffix']
+
+	print "dom" + dom
+	print "subd" + sub
+	print "tld" + tld
+
+	if (dom in BLACKLISTED_SITES
+		or url in BLACKLISTED_SITES
+		or '.'.join([sub, dom, tld]) in BLACKLISTED_SITES
+		or '.'.join([sub, dom]) in BLACKLISTED_SITES
+		or '.'.join([dom, tld]) in BLACKLISTED_SITES
+		or '.'.join([dom]) in BLACKLISTED_SITES):
+		print "Blacklisted!"
+		return True
+
+	print "Not blacklisted!!!"
+	return False
+
+
 def parseTweet(tweet):
-	print 'yipee!'
+	print tweet.urls
+	urls = tweet.urls
+	for urlObject in urls:
+		url = urlObject['expanded_url']
+		print url
+		if not appearsInBlacklist(url):
+			api.update_status(url)
 
 #Tweet class with all the information we need for this program (Hashtags and the actual tweet text)
 class Tweet:
-    text = str()
+	text = str()
 	hashtags = []
+	urls = []
 
-    def __init__(self, json):
-		self.text = json["text"] 
-		self.hashtags = json["entities"]["hashtags"]
+	def __init__(self, json):
+		self.text = json.get('text', '')
+		self.hashtags = json.get('entities', {}).get('hashtags', [])
+		self.urls = json.get('entities', {}).get('urls', [])
 
 #Basic listener which parses the json, creates a tweet, and sends it to parseTweet
 class TweetListener(StreamListener):
@@ -68,6 +117,5 @@ if __name__ == '__main__':
 	parsedTweets = 0
 	listener = TweetListener()
 	stream = Stream(auth, listener)	
-	stream.filter(track=[BOT_NAME]) #This will start the stream and make callbacks to the listener for all tweets containing "android"
-
+	stream.filter(track=[BOT_NAME])
 
